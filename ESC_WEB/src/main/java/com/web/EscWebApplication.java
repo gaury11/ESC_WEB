461c577da612f44928fd609fe43d96d692a4927b
soon <soon@192.168.0.180>
2019-04-25 20:44:02 +0900

    ESC_WEB 최초 커

---
+++ ESC_WEB/src/main/java/com/web/EscWebApplication.java
@@ -0,0 +1,13 @@
+package com.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscWebApplication.class, args);
+	}
+
+}
